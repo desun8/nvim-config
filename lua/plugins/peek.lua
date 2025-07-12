b01e8d7f46737108437e6b3157f4c1d47abe9080
Evgenii Pchelinov <evgenii@Evgeniis-MacBook-Pro.local>
2025-07-12 13:17:31 +0500

    Отключает peek.nvim

--- lua/plugins/peek.lua
+++ lua/plugins/peek.lua
@@ -1,15 +1,16 @@
 -- Просмотр отрендеренного markdown
+-- Отключил, не хочу ставить deno ради одного плагина
 return {
-	{
-		"toppair/peek.nvim",
-		event = { "VeryLazy" },
-		build = "deno task --quiet build:fast",
-		config = function()
-			require("peek").setup({
-				filetype = { "markdown", "conf" },
-			})
-			vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-			vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
-		end,
-	},
+	-- {
+	-- 	"toppair/peek.nvim",
+	-- 	event = { "VeryLazy" },
+	-- 	build = "deno task --quiet build:fast",
+	-- 	config = function()
+	-- 		require("peek").setup({
+	-- 			filetype = { "markdown", "conf" },
+	-- 		})
+	-- 		vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+	-- 		vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+	-- 	end,
+	-- },
 }
